6a0f69f66343f91315c2a6442dd3049edd594cc8
rusyaevsa <rusyaev@mail.ru>
2020-07-04 16:11:44 +0400

    connected api and added celery

--- aqi_monitor/apps/monitor/tasks.py
+++ aqi_monitor/apps/monitor/tasks.py
@@ -1,9 +1,22 @@
 from __future__ import absolute_import, unicode_literals
-import time
+
+from celery.schedules import crontab
 from aqi_monitor.celery import app
+import requests
+import json
+
+app.conf.beat_schedule = \
+    {"send_message": {
+        "task": "apps.monitor.tasks.get_aqi",
+        "schedule": crontab(minute="*/1"),
+        "args": (),
+    }}
 
 
-@app.task(bind=True)
-def celery_task(self, counter):
-    time.sleep(3)
-    return '{} Done!'.format(counter)
+@app.task
+def get_aqi():
+    region = "saratov"
+    response = requests.get(
+        "https://api.waqi.info/feed/" + region + "/?token=7162b8553552c91543ed0ae44c44e59b3724a08e")
+    result = json.loads(response.text)
+    print(result)
--- aqi_monitor/apps/monitor/views.py
+++ aqi_monitor/apps/monitor/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import HttpResponse
 from django.views.generic import View
-from .tasks import celery_task
 
 
 class MyView(View):
-
     def get(self, request):
-        result = celery_task(1)
-        return HttpResponse(result)
+        pass
