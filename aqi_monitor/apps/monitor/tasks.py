2d7c60976f493c802145f524d27b2880acd43437
NadyaSukhova <48157797+NadyaSukhova@users.noreply.github.com>
2020-07-10 19:09:48 +0400

    Task group implemented
    * fix parse-file, add to requirenments, add parent task in tasks.py
    
    * change psycopg2 version

--- aqi_monitor/apps/monitor/tasks.py
+++ aqi_monitor/apps/monitor/tasks.py
@@ -4,22 +4,28 @@ import requests
 import json
 
 from celery.schedules import crontab
+from celery import group
 from aqi_monitor.celery import app
 from . import parse_aqi_data
 
 
 app.conf.beat_schedule = \
     {"send_message": {
-        "task": "apps.monitor.tasks.get_aqi",
-        "schedule": crontab(minute="*/15"),
+        "task": "apps.monitor.tasks.extract_aqi",
+        "schedule": crontab(minute="*/1"),
         "args": (),
     }}
 
+@app.task
+def extract_aqi():
+    regions_list = ["moscow", "paris", "new%20york", "saratov", "berlin", "tokyo"]
+    tasks_list = [send_request.s(i) for i in regions_list]
+    tasks_list = group(*tasks_list).apply_async()
+
 
 @app.task
-def get_aqi():
-    region = "moscow"
+def send_request(region):
     response = requests.get(
-        f"https://api.waqi.info/feed/{region}/?token=7162b8553552c91543ed0ae44c44e59b3724a08e")
+        f"https://api.waqi.info/feed/{region}/?token=d03d6e3baa62248fd0f86b3df1c70b211ed08f35")
     data = json.loads(response.text)["data"]
     aqi_data_result = parse_aqi_data.parse_aqi_data(data)
